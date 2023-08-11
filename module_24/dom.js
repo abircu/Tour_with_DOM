1a6bfa85bef1d65f45df4be951ba476f92981ae9
abircu <mdabircse4873@gmail.com>
2023-08-12 00:57:14 +0600

    js dom practice using classname,tagname,and id

--- module_24/dom.js
+++ module_24/dom.js
@@ -7,3 +7,9 @@ const allHeadings = document.getElementsByTagName("h1");
 for (const h1 of allHeadings) {
   console.log(h1.innerText);
 }
+const allFruits = document.getElementsByClassName("fruits-name");
+for (const fruits of allFruits) {
+  console.log(fruits.innerText);
+}
+const otherClass = document.getElementsByClassName("others");
+console.log(otherClass.innerText);
